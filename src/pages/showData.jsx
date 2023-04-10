b875a6c2e6c8cbad8226354c66f82ad225363ded
Gr3st <118692156+Gr3st@users.noreply.github.com>
2023-04-10 09:03:38 +0000

    big-change

--- src/pages/showData.jsx
+++ src/pages/showData.jsx
@@ -37,3 +37,48 @@ export default function ShowData(props) {
         </div>
     );
 }
+
+// import { firestore } from "../firebase";
+// import { collection, doc, getDocs, query, where, onSnapshot } from "@firebase/firestore";
+// import React, { useState, useEffect } from "react";
+// import { auth } from "../firebase";
+// import { useAuthState } from 'react-firebase-hooks/auth';
+
+// export default function ShowData(props) {
+//     const [text, setText] = useState("");
+//     const [lang, setLang] = useState("");
+//     const [user] = useAuthState(auth);
+
+//     const getData = async () => {
+//         const userCollectionRef = collection(firestore, "users");
+//         const q = query(userCollectionRef, where("uid", "==", user.uid));
+//         const querySnapshot = await getDocs(q);
+
+//         const collectionRef = collection(firestore, "Translator");
+//         const docRef = doc(collectionRef, querySnapshot.docs[0].data().server);
+        
+//         // Listen for realtime updates
+//         const unsubscribe = onSnapshot(docRef, (docSnap) => {
+//             if (docSnap.exists()) {
+//                 const data = docSnap.data();
+//                 setText(data.text);
+//                 setLang(querySnapshot.docs[0].data().lang);
+//             }
+//         });
+
+//         // Unsubscribe from realtime updates when the component unmounts
+//         return unsubscribe;
+//     };
+
+//     useEffect(() => {
+//         getData();
+//     }, [props.state]);
+
+//     return(
+//         <div>
+//             {text !== "" ? text : ""}
+//             <br />
+//             {<div><span>Lang</span><br />{lang}</div>}
+//         </div>
+//     );
+// }
