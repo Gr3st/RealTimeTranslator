ee82512a93ca973c40b1db65d49758e5b4e708c0
Gr3st <118692156+Gr3st@users.noreply.github.com>
2023-04-10 07:34:30 +0000

    send3

--- src/pages/sendData.jsx
+++ src/pages/sendData.jsx
@@ -1,21 +1,32 @@
 import Axios from "axios";
 import { useState, useEffect } from "react";
-import { getDocs, collection, doc, setDoc } from "firebase/firestore";
+import { getDocs, collection, doc, setDoc, query, where } from "firebase/firestore";
 import { firestore } from "../firebase";
 import { auth } from "../firebase";
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from "react-router-dom";
 
 export default function SendData() {
   const [s, setS] = useState("");
   const [da, setDa] = useState("");
   const [user] = useAuthState(auth);
+  const [lang, setLang] = useState("");
+
+  const navigate = useNavigate();
   const la = "en";
- 
+
+  const conectionDB = async () =>{
+    const userCollectionRef = collection(firestore, "users");
+    const q = query(userCollectionRef, where("uid", "==", user.uid));
+    const querySnapshot = await getDocs(q);
+    setLang(querySnapshot.docs[0].data().lang);
+  };
+
   useEffect(() => {
-    
+    conectionDB();
     if (s !== "" & s.length < 500) {
       Axios.get(
-        `https://api.mymemory.translated.net/get?q=${s}&langpair=${props.lang}|${la}`
+        `https://api.mymemory.translated.net/get?q=${s}&langpair=${lang}|${lang=="en"?"pl":la}`
       ).then((res) => {
         setDa(res.data.responseData.translatedText);
         console.log(s.length);
@@ -25,6 +36,7 @@ export default function SendData() {
   }, [s]);
   
   const send = async () => {
+    
     const userCollectionRef = collection(firestore, "users");
     const q = query(userCollectionRef, where("uid", "==", user.uid));
     const querySnapshot = await getDocs(q);
@@ -41,6 +53,7 @@ export default function SendData() {
     try {
       await setDoc(docRef, data);
       console.log("Send to ID:", docRef.id);
+      navigate("/");
     } catch (error) {
       console.error("Error adding document:", error);
     }
